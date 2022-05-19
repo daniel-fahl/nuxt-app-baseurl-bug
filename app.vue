f51071140315b2133fb1b857a00604bdec52e8aa
Daniel Fahl <git@daniel-fahl.de>
2022-05-19 10:53:24 +0200

    first commit

---
+++ app.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <h1>
+      Hello {{ baseURL }}
+    </h1>
+    <a href="/test/">Go to /test/</a>
+  </div>
+</template>
+
+<script setup>
+const baseURL = process.env.NUXT_APP_BASE_URL;
+</script>
+
+<style>
+html, body {
+  height: 100%;
+  margin: 0;
+}
+
+div {
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+</style>
